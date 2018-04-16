399d61885fc8eda3224732a1e35535eeea3e3978
vitornascimentoaraujo@gmail.com <vitornascimentoaraujo@gmail.com>
2018-04-16 00:11:51 -0300

    create_app function pattern to initialize app flask

--- app/bases/__init__.py
+++ app/bases/__init__.py
@@ -2,6 +2,6 @@ from os import environ
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 
-env = 'master' if environ.get('IS_HEROKU', None) else 'develop'
+env = environ.get('IS_HEROKU', 'develop')
 db = SQLAlchemy()
 base_schema = Marshmallow()
