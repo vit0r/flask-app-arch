2a23aa3237343c593bebebcdf03aef9c2fd73191
vitornascimentoaraujo@gmail.com <vitornascimentoaraujo@gmail.com>
2018-04-16 12:39:58 -0300

    create_app function pattern to initialize app flask

--- app/routes/user_route.py
+++ app/routes/user_route.py
@@ -12,6 +12,6 @@ user_bp = Blueprint(name='user_bp', import_name=__name__, url_prefix='/api')
 @user_bp.route('/users', methods=['GET'])
 def find_all():
     users = UserModule().find_all()
-    if not users.data:
+    if not users:
         return None, status.HTTP_204_NO_CONTENT
     return {'data': users.data}, status.HTTP_200_OK
