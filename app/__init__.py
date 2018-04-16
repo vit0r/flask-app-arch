9aae84cddfea14758f67b07d33694cc74b121a78
vitornascimentoaraujo@gmail.com <vitornascimentoaraujo@gmail.com>
2018-04-15 22:20:18 -0300

    create_app function pattern to initialize app flask

--- app/__init__.py
+++ app/__init__.py
@@ -10,15 +10,18 @@ from app.routes.user_route import user_bp
 
 __author__ = 'Vitor Nascimento de Araujo'
 
-__instance_path = path.join(path.dirname(__file__), 'instances', env + '.json')
-app = FlaskAPI(__name__, instance_path=__instance_path, instance_relative_config=True)
-app.config.from_json(__instance_path)
-app.url_map.strict_slashes = False
 
-db.init_app(app)
-base_schema.init_app(app)
+def create_app():
+    __instance_path = path.join(path.dirname(__file__), 'instances', env + '.json')
+    app = FlaskAPI(__name__, instance_path=__instance_path, instance_relative_config=True)
+    app.config.from_json(__instance_path)
+    app.url_map.strict_slashes = False
 
-app.register_blueprint(base_bp)
-app.register_blueprint(user_bp)
+    db.init_app(app)
+    base_schema.init_app(app)
 
-db.create_all(app=app)
+    app.register_blueprint(base_bp)
+    app.register_blueprint(user_bp)
+
+    db.create_all(app=app)
+    return app
--- run.py
+++ run.py
@@ -2,9 +2,10 @@
 cop-op marketing
 """
 
-from app import app
+from app import create_app
 
 __author__ = 'Vitor Nascimento de Araujo'
 
 if __name__ == '__main__':
+    app = create_app()
     app.run()
